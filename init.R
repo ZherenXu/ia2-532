9fcba7d732af008fab476486578833b929474919
Zheren Xu <xuzheren1998@gmail.com>
2022-03-09 18:16:48 -0800

    change init.R

--- init.R
+++ init.R
@@ -9,5 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes', 'tidyverse', 'purrr'))
+install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes', 'tidyverse', 'purrr', 'dashCoreComponents', 'dashHtmlComponents', 'dashBootstrapComponents'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
